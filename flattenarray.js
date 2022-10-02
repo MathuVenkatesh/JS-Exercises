849dd59f646381eedbd0e2dc534bee926bd5718c
vengateshkr vengat <vengateshkr@gmail.com>
2022-10-02 19:19:03 +0530

    changed 1st file

--- flattenarray.js
+++ flattenarray.js
@@ -1,25 +1,3 @@
-//Flatten Array
-
-function flattednArr(inputArr) {
-  let outputArr = [];
-  recursion(0, inputArr, outputArr);
-  return outputArr;
-}
-
-function recursion(index, inputArr, outputArr) {
-  if (index >= inputArr.length) return;
-
-  if (Array.isArray(inputArr[index])) {
-    recursion(0, inputArr[index], outputArr);
-  } else {
-    outputArr.push(inputArr[index]);
-  }
-  recursion(index + 1, inputArr, outputArr);
-}
-
-out = flattednArr([1, 2, 3, [4, [5, 6]], [7, 8, [9, 10]], [11, 12]]);
-console.log(out);
-
 // What is between? (1,5) output=> [1,2,3,4,5]
 //(-2,2) output=> [-2,-1,0,1,2]
 
